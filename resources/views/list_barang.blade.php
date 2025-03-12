30706b615d79f1787779ae6fddf697a4af4808e9
Muhammad Danial <mhd2danial3@gmail.com>
2025-03-12 14:56:02 +0700

    Konfigurasi Route View dan Controller P3

---
+++ resources/views/list_barang.blade.php
@@ -0,0 +1,8 @@
+<html>
+    <div>
+        <!-- Be present above all else. - Naval Ravikant -->
+        <h1>List Barang</h1>
+        <p>Kode Barang: {{$id}}</p>
+        <p>Nama Barang: {{$nama}}</p>
+    </div>
+</html>
